8c5ebe9299c6b290e7e794f1af703773d8277985
abrahamjeron <abrahamjeron40@gmail.com>
2024-04-29 12:57:22 +0530

    connected mongodb

---
+++ Backend/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const connectDb = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+    }catch(err){
+        console.error(err)
+    }
+}
+
+const checkconnected = ()=>{
+    const dbStatus = mongoose.connection.readyState;
+    return dbStatus === 1; 
+}
+module.exports={
+    connectDb,
+    checkconnected
+}
--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,27 @@
-// Included all dependencies
-const express = require('express')
-require('dotenv').config();
-const app=express()
-// Created an separate endpoint to test the server
-app.get('/test',(req,res)=>{
-    res.send('server running!');
-})
-// included the port from env file
-const port = process.env.PORT || 3000
-app.listen(port,()=>{
-    console.log(`server is runnig on Port:${port}...`)
-})
+const express = require('express');
+const dotenv = require('dotenv').config(); 
+const mongoose = require('mongoose');
+const routes = require('./routes'); 
+
+const app = express();
+app.use('/api', routes);
+
+const { connectDb,checkconnected } = require('./db');
+connectDb();
+
+app.get('/test', (req, res) => {
+    res.send('Server running!');
+});
+
+app.get('/', (req, res) => {
+    if (checkconnected()) {
+        res.send('Database connection status: Connected');
+    } else {
+        res.send('Database connection status: Connection failed');
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}...`);
+});
