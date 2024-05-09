ea1d3630bab239be2f3b1cdd4a9ee13abee810d3
abrahamjeron <abrahamjeron40@gmail.com>
2024-05-09 15:30:11 +0530

    get api used

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const connectDb = async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI)
     }catch(err){
         console.error(err)
     }
