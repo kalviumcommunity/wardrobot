8693a05185f5f6e5fb7d5b3d2f79eaa70414d5a3
abrahamjeron <abrahamjeron40@gmail.com>
2024-05-16 12:32:30 +0530

    connected backed for user Authentication

---
+++ Frontend/wardrobot /src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Loader.css'; 
+function Loader() {
+    return (
+        <div className="loader-container">
+            <div className="loader"></div>
+        </div>
+    );
+}
+
+export default Loader;
--- Frontend/wardrobot /src/components/login.jsx
+++ Frontend/wardrobot /src/components/login.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
 import logo from "./assets/logo.png";
-import downbtn from "./assets/DownButton.png";
+import Cookies from 'js-cookie';
+import axios from 'axios'
 import { Link } from 'react-router-dom';
 import profile from "./assets/profile.png"
 import ellipse from "./assets/Ellipse.png";
 import { useNavigate } from 'react-router-dom';
+import Loader from "./Loader.jsx";
 function Login() {
+    const [userName,setUserName]=useState('')
+    const [password,setPassword]=useState('')
+    const [loading, setLoading] = useState(false); 
     const navigate = useNavigate()
     const handleBack=()=>{
-        navigate('/')
+        navigate('/home')
     }
-    const handleLogin=()=>{
-        navigate('/setup1')
-    }
+    const removeAllCookies = () => {
+        const cookies = Cookies.get();
+        for (const cookie in cookies) {
+            Cookies.remove(cookie);
+        }
+    };
+    // removeAllCookies()
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        try {
+            const userData = await axios.get(`http://localhost:3000/users/getuserbyUsername/${userName}`);
+            const fetchedPassword=userData.data[0].password;
+            console.log(fetchedPassword)
+            if (fetchedPassword===password){
+                if (userData.status === 201) {
+                    Cookies.set('userName', userName, { expires: 7 });
+                    console.log(userData);
+                    navigate('/home');
+                }
+            }else{
+                alert('incorrect password or username')
+            }
+        } catch (err) {
+            console.log(err);
+            alert('incorrect password or username')
+        }finally {
+            setLoading(false); 
+        }
+    };    
+
     return (
         <>
             <img src={logo} alt="" id="logo2" />
@@ -22,9 +55,11 @@ function Login() {
             <div className='form'>
                 <div className="login wrap">
                     <div className="h1">Login</div>
-                    <input pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" placeholder="Email" id="email" name="email" type="text" />
-                    <input placeholder="Password" id="password" name="password" type="password" />
-                    <input value="Login" className="btn" type="submit" onClick={handleLogin} />
+                    <input pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" placeholder="Email" id="email" name="email" type="text" onChange={e=> setUserName(e.target.value)} />
+                    <input placeholder="Password" id="password" name="password" type="password" onChange={e=> setPassword(e.target.value)} />
+                    <button className="btn" type="submit" onClick={handleLogin} disabled={loading}>
+                        {loading ? <Loader /> : "Signup"} 
+                    </button>
                     <div id='signup-nav'>
                         <p >Don't have an account? <Link to='/signup'>Sign up</Link></p>
                     </div>
